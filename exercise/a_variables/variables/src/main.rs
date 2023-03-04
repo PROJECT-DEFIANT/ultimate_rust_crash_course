db39ac452acfd5b0f7eaef6e18776c1bff228609
PROJECT-DEFIANT <szymon.szyszkowski@mnm.bio>
2023-03-04 07:31:09 +0100

    challenge: variables

--- exercise/a_variables/variables/src/main.rs
+++ exercise/a_variables/variables/src/main.rs
@@ -1,3 +1,12 @@
+const MISSILES: i32 = 8;
+const READY: i32 = 2;
 fn main() {
-    println!("Hello, world!");
+    println!("Fireing of my {} missiles, when {}", MISSILES, READY);
+    let missiles = MISSILES -  READY;
+
+    let (mut x, y) = (4,5);
+    x = x - y;
+
+    println!("x:{},y:{}",x,y);
+    println!("Remaining missiles {}", missiles);
 }
